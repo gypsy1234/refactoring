d50d74b91ab21927cfd2881c8668e947da437287
貴浩 安齋 <gypsy.gipsy.1018@gmail.com>
2018-12-30 17:43:27 +0900

    第1章　料金算出のメソッドをRentalクラスに移動 メソッド内でそれが定義されているクラスのデータを使用していない場合、そのメソッドは間違ったクラスに割り当てられている

--- src/main/java/part1/Customer.java
+++ src/main/java/part1/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
         for (Rental rental : _rentals) {
-            double thisAmount = amountFor(rental);
+            double thisAmount = rental.getCharge();
 
             // レンタルポイントを加算
             frequentRenterPoints++;
@@ -40,26 +40,6 @@ public class Customer {
         // フッタ部分の追加
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
-        return result;
-    }
-
-    private double amountFor(Rental rental) {
-        double result = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2)
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (rental.getDaysRented() > 3)
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
         return result;
     }
 }
