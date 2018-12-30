615e9525d100d7c0ce5e3671530cccd15181cf8c
貴浩 安齋 <gypsy.gipsy.1018@gmail.com>
2018-12-30 18:03:05 +0900

    第1章　合算料金の算出をメソッド抽出 一時変数の削除

--- src/main/java/part1/Customer.java
+++ src/main/java/part1/Customer.java
@@ -21,7 +21,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
         for (Rental rental : _rentals) {
@@ -29,12 +28,19 @@ public class Customer {
             frequentRenterPoints += rental.getFrequentRenterPoints();
             // この貸し出しに関する数値の表示
             result += "\t" + rental.getMovie().getTitle() + "\t" + rental.getCharge() + "\n";
-            totalAmount += rental.getCharge();
         }
 
         // フッタ部分の追加
-        result += "Amount owed is " + totalAmount + "\n";
+        result += "Amount owed is " + getTotalCharge() + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        for (Rental rental : _rentals) {
+            result += rental.getCharge();
+        }
         return result;
     }
 }
